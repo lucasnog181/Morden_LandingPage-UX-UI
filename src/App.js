a6a5eb7750920fec953e988dfa37d03af2adc59f
lucasnog181 <lucasnog17@edu.unifor.br>
2021-11-21 12:10:41 -0300

    finish commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+
+//import Articles from './components/articles/Article';
+import Brand from './components/brand/Brand';
+import CTA from './components/cta/CTA';
+//import Features from './components/feature/Feature';
+import Navbar from './components/navbar/Navbar';
+
+import Blog from './containers/blog/Blog';
+import Features from './containers/features/Features';
+import Footer from './containers/footer/Footer';
+import Header from './containers/header/Header';
+import Possibility from './containers/possibility/Possibility';
+import WthatGPT3 from './containers/whatGPT3/WhatGPT3';
+
+
+import './App.css'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WthatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
     </div>
   );
 }
 
 export default App;
+
